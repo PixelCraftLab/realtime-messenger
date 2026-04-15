f361f23be3deb8b04cb7bc328744e233799e5535
HariRudraPratapSingh <harirudra.singh123@gmail.com>
2026-04-15 23:52:43 +0530

    resolved merge

---
+++ realtime-messenger/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="text-white h-screen w-full bg-neutral-600">
+  <div className="">
+    Chats
+  </div>
+</div>
+  )
+}
+
+export default App
