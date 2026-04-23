a41bf3f8643b65be1f846f3ac44117cabbeaa6cb
Shanmukha Sai <c.shanmukha.7@gmail.com>
2026-04-23 12:14:34 +0530

    Added WhatsApp-style chat header

--- src/Pages/ChatPage.jsx
+++ src/Pages/ChatPage.jsx
@@ -1,9 +1,19 @@
-import ChatWindow from "../components/ChatWindow";
-import MessageInput from "../components/MessageInput";
+import ChatWindow from "../Components/ChatWindow";
+import MessageInput from "../Components/MessageInput";
+
 
 const ChatPage = () => {
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col h-screen bg-[#0b141a]">
+
+      {/* Header */}
+      <div className="flex items-center justify-between px-4 py-3 bg-[#202c33]">
+        <div>
+          <h2 className="text-white font-semibold">Rahul</h2>
+          <p className="text-xs text-gray-400">online</p>
+        </div>
+      </div>
+
       <ChatWindow />
       <MessageInput />
     </div>
