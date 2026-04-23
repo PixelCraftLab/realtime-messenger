e2a889e25f2530d1addd15764bd6fb6ae07fc7f0
Shanmukha Sai <c.shanmukha.7@gmail.com>
2026-04-23 12:06:48 +0530

    Added typing indicator UI

---
+++ src/Components/TypingIndicator.jsx
@@ -0,0 +1,11 @@
+const TypingIndicator = () => {
+  return (
+    <div className="flex justify-start">
+      <div className="bg-[#202c33] px-4 py-2 rounded-lg text-sm text-gray-300">
+        typing...
+      </div>
+    </div>
+  );
+};
+
+export default TypingIndicator;
