8d231a05ba03970e384e6a5a359903f4dec502d8
Shanmukha Sai <c.shanmukha.7@gmail.com>
2026-04-23 11:55:40 +0530

    Connected PersonalDashboard with ChatContext

--- src/Components/PersonalDashBoard.jsx
+++ src/Components/PersonalDashBoard.jsx
@@ -1,3 +1,6 @@
 import React, { useContext } from "react";
 import { FiMessageSquare, FiUsers, FiStar } from "react-icons/fi";
 import { ChatContext } from "../context/ChatContext";
+const PersonalDashBoard = () => {
+  const { messages, scheduled } = useContext(ChatContext);
+}
