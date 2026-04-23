a4ecf9a9bfb5279ddff40a881d729b65902eb9f8
Shanmukha Sai <c.shanmukha.7@gmail.com>
2026-04-23 11:56:03 +0530

    Implemented dashboard analytics (total messages, active chats, top contact)

--- src/Components/PersonalDashBoard.jsx
+++ src/Components/PersonalDashBoard.jsx
@@ -3,4 +3,19 @@ import { FiMessageSquare, FiUsers, FiStar } from "react-icons/fi";
 import { ChatContext } from "../context/ChatContext";
 const PersonalDashBoard = () => {
   const { messages, scheduled } = useContext(ChatContext);
+    const totalMessages = messages.length;
+
+  const activeChats = new Set(messages.map(m => m.contactId)).size;
+
+  const freq = {};
+  messages.forEach(m => {
+    freq[m.contactId] = (freq[m.contactId] || 0) + 1;
+  });
+
+  const topContact =
+    Object.keys(freq).length === 0
+      ? "None"
+      : Object.keys(freq).reduce((a, b) =>
+          freq[a] > freq[b] ? a : b
+        );
 }
